0b4482b5e0e45caa2b5b7e3ad36fb8cf643f30e8
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-05-11 01:02:23 +0200

    création des pages

---
+++ src/pages/About.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propops</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque odit sed
+        alias non consequatur a aliquid dignissimos? Iure ex minus perspiciatis
+        asperiores ipsum voluptate doloremque exercitationem, quasi et, nemo
+        obcaecati! Consectetur, alias delectus, in eum et obcaecati laborum
+        debitis porro sapiente libero odio omnis ut voluptatibus, animi numquam
+        autem aspernatur eveniet laboriosam ex ad ipsum? Numquam, id ab.
+        Consequuntur at reprehenderit commodi nihil in totam vero. Suscipit ipsa
+        iusto modi rem, eum minima quidem facere sequi commodi omnis fugiat.
+        Neque labore, nemo doloribus accusamus facilis numquam consequuntur
+        temporibus suscipit officiis quisquam eos dolore cupiditate placeat
+        rerum possimus amet voluptatem vero?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ipsa ab
+        illum consectetur necessitatibus quam voluptate culpa quidem rerum
+        debitis, distinctio veritatis ut fugit sit, eos ipsam? In dolor quas ad,
+        id dignissimos doloribus! Placeat rem hic exercitationem fugit
+        aspernatur repellendus quas inventore quo error voluptatem? Sequi
+        dolorem commodi soluta id placeat ducimus nobis quo tempora quia dolore
+        quae amet nihil similique eligendi, optio voluptates unde. Ullam fugit
+        amet alias expedita iste quam perferendis ex? Velit asperiores nulla
+        inventore molestiae a perspiciatis, ullam voluptatibus error dolore.
+        Corrupti, molestiae nemo! Cum, perspiciatis modi. Minus accusantium,
+        sint, iure quaerat cum illo commodi unde excepturi, quis sapiente
+        inventore alias eos accusamus rem perspiciatis facilis natus perferendis
+        qui enim. Assumenda optio nihil neque magnam reiciendis recusandae,
+        corporis laboriosam dolores iste, placeat alias asperiores at cum
+        inventore consectetur fuga, quo dolore debitis laborum esse excepturi
+        accusantium doloremque. Iure optio saepe vero omnis perspiciatis
+        tenetur. Aliquid, deleniti odit natus dicta fugit assumenda tempora!
+        Tenetur illo aspernatur architecto consectetur assumenda, corrupti sequi
+        dignissimos perspiciatis ab iusto, quasi reiciendis dolor? Repudiandae
+        dicta excepturi harum odio exercitationem, facere eligendi autem, modi
+        dolorum, ducimus doloremque eveniet. Blanditiis dicta facilis, aliquid
+        sunt explicabo magni possimus debitis praesentium dolores et odio sed?
+      </p>
+    </div>
+  );
+};
+
+export default About;
