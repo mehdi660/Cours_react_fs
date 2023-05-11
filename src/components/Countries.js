0473194f0a457607555bad722fd5efb3b678f797
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-05-11 18:12:42 +0200

    ajout des drapeau des pays, leurs noms, leurs capitales et leurs population

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./Card";
 
 const Countries = () => {
+  const [data, setData] = useState([]);
+  // Le useEffect se joue lorsque le composant est monté
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setData(res.data));
+  }, []);
+
   return (
     <div className="countries">
-      <h1>COUNTRIES</h1>
+      <ul className="radio-container">
+        <input type="range" min="1" max="250" />
+      </ul>
+      <ul>
+        {data.slice(0, 15).map((country, index) => (
+          <Card key={index} country={country} />
+        ))}
+      </ul>
     </div>
   );
 };
