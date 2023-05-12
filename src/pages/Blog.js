3a7cd73fda7434123035b49a548c84acde0daa31
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-05-12 19:38:05 +0200

    ajt de nouveaux fichier pour la partie 2 du projet

---
+++ src/pages/Blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../components/Logo";
+
+const Blog = () => {
+  return (
+    <div className="blog-container">
+      <Logo />
+    </div>
+  );
+};
+
+export default Blog;
