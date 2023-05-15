14d3509c9e74b2783c37c045692702f64a6d2b57
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-05-15 16:09:03 +0200

    paramétrage du formulaire pour l'envoi des messages

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,15 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
+import axios from "axios";
+import Article from "../components/Article";
 
 const Blog = () => {
+  const [author, setAuthor] = useState("");
+  const [blogData, setBlogData] = useState([]);
   const [content, setContent] = useState("");
   const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const getData = () => {
+      axios
+        .get("http://localhost:3004/articles")
+        .then((res) => setBlogData(res.data));
+    };
+    getData();
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (content.length < 140) {
       setError(true);
+    } else {
+      axios.post("http://localhost:3004/articles", {
+        author,
+        content,
+        date: Date.now(),
+      });
+      setError(false);
     }
   };
 
@@ -20,15 +41,26 @@ const Blog = () => {
       <h1>Blog</h1>
 
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="Nom" />
+        <input
+          type="text"
+          placeholder="Nom"
+          onChange={(e) => setAuthor(e.target.value)}
+        />
         <textarea
+          style={{ border: error ? "1px solid red" : "1px solid #61dafb" }}
           placeholder="Message"
           onChangeCapture={(e) => setContent(e.target.value)}
         ></textarea>
         {error && <p>Veuillez ecrire un minimum de 140 caractéres</p>}
         <input type="submit" value="Envoyer" />
       </form>
-      <ul></ul>
+      <ul>
+        {blogData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+      </ul>
     </div>
   );
 };
