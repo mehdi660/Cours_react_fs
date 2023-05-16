d377af30fe9fc3fd2b7f8a257a9338a6cb03f01d
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-05-16 15:32:39 +0200

    finition du creud de la partie blog

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,6 +1,9 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-const article = ({ article }) => {
+const Article = ({ article }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editContent, setEditContent] = useState("");
   const dateFormater = (date) => {
     let newDate = new Date(date).toLocaleDateString("fr-FR", {
       year: "numeric",
@@ -12,20 +15,65 @@ const article = ({ article }) => {
     });
     return newDate;
   };
-  console.log(article);
+
+  const handleEdit = () => {
+    const data = {
+      author: article.author,
+      content: editContent ? editContent : article.content,
+      date: article.date,
+      updatedDate: Date.now(),
+    };
+
+    axios.put("http://localhost:3004/articles/" + article.id, data).then(() => {
+      setIsEditing(false);
+    });
+    setIsEditing(false);
+  };
+
+  const handleDelete = () => {
+    axios.delete("http://localhost:3004/articles/" + article.id);
+    window.location.reload();
+  };
+
   return (
-    <div className="article">
+    <div
+      className="article"
+      style={{ background: isEditing ? "#f3feff" : "white" }}
+    >
       <div className="card-header">
         <h3>{article.author}</h3>
         <em>Posté le {dateFormater(article.date)}</em>
       </div>
-      <p>{article.content}</p>
+      {isEditing ? (
+        <textarea
+          defaultValue={editContent ? editContent : article.content}
+          autoFocus
+          onChange={(e) => setEditContent(e.target.value)}
+        ></textarea>
+      ) : (
+        <p>{editContent ? editContent : article.content}</p>
+      )}
       <div className="btn-container">
-        <button>Edit</button>
-        <button>Supprimer</button>
+        {isEditing ? (
+          <button onClick={() => handleEdit()}>Valider</button>
+        ) : (
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+        )}
+
+        <button
+          onClick={() => {
+            if (
+              window.confirm("Voulez vous vraimment supprimer cet article ?")
+            ) {
+              handleDelete();
+            }
+          }}
+        >
+          Supprimer
+        </button>
       </div>
     </div>
   );
 };
 
-export default article;
+export default Article;
